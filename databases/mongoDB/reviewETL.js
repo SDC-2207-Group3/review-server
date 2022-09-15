938d224303061320720213cd971f0afb6a94fd4b
Thach Do <thachdo90@gmail.com>
2022-09-15 10:52:15 -0700

    tested out parsing and transforming reviews csv

--- databases/mongoDB/reviewETL.js
+++ databases/mongoDB/reviewETL.js
@@ -1,11 +1,26 @@
 const csv = require('csv-parser');
 const fs = require('fs');
-const results = [];
+
+const reviews = {};
 
 //use promises to make sure all the data is here before we start putting them together
-fs.createReadStream('/Users/thachdo/Documents/RFP2207/rfp2207-fec/oldData/characteristic_reviews.csv')
+fs.createReadStream('/Users/thachdo/Documents/RFP2207/review-server/oldData/reviews.csv')
   .pipe(csv())
-  .on('data', (data) => results.push(data))
+  .on('data', (data) => reviews[data.id] = {
+    product_id: parseInt(data.product_id),
+    rating: parseInt(data.rating),
+    summary: data.summary,
+    recommend: Boolean(data.recommend),
+    response: data.response === 'null' ? null : data.response,
+    body: data.body,
+    date: Date(data.date),
+    reported: Boolean(data.reported),
+    reviewer_name: data.reviewer_name,
+    helpfulness: parseInt(data.helpfulness),
+    photos: []
+  })
   .on('end', () => {
-    console.log(results.slice(0, 5));
+    for (let i = 0 ; i < 5; i++) {
+      console.log(reviews[i]);
+    }
   })

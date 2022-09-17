35c77135f9af5e9f4006e232d53f755a51e281d3
Thach Do <thachdo90@gmail.com>
2022-09-16 20:08:30 -0700

    added limit to query and added alternative to use aggregation

--- server/models.js
+++ server/models.js
@@ -6,8 +6,16 @@ const retrieve = async(product_id, page, count, cb) => {
   // take out product_id
   // change photos _id to id
   // we can iterate through it here in JS or use mongoDB/mongoose methods
-  Review.find({"product_id": product_id}, cb)
+  Review.find({"product_id": product_id}, cb).limit(count)
+
+
 }
 
 
 export { retrieve }
+
+// working example using aggregation
+// let filter = {product_id: parseInt(product_id)}
+// let result = await Review.aggregate([{$match: filter}])
+// console.log('got result', result)
+// cb(null, result);
